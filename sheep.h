f7c0acc1fd51591d039c348e75a42b9cf2934c11
FrancescoGnerre <47790362+FrancescoGnerre@users.noreply.github.com>
2019-05-12 16:05:44 -0700

    Add files via upload
    Added the sheep class

---
+++ sheep.h
@@ -0,0 +1,57 @@
+#ifndef S_H
+#define S_H
+
+#include <iostream>
+#include "TexRect.h"
+
+class Sheep: public TexRect {
+
+    bool up, down, left, right;
+    int upK, downK, leftK, rightK;
+
+public:
+
+   // Player(float x, float y, float w, float h, float r, float g, float b);
+    Sheep(const char* filename, float x, float y, float w, float h, int upK, int rightK, int downK, int leftK): TexRect(filename, x, y, w, h) {
+        up = false;
+        down = false;
+        left = false;
+        right = false;
+
+        this->upK = upK;
+        this->rightK = rightK;
+        this->downK = downK;
+        this->leftK = leftK;
+    }
+
+    void move(int key) {
+        if(key == leftK) {
+            left = true;
+        }
+        if(key == upK) {
+            up = true;
+        }
+        if(key == rightK) {
+            right = true;
+        }
+        if(key == downK) {
+            down = true;
+        }
+    };
+
+    void stop(int key) {
+        if(key == leftK) {
+            left = false;
+        }
+        if(key == upK) {
+            up = false;
+        }
+        if(key == rightK) {
+            right = false;
+        }
+        if(key == downK) {
+            down = false;
+        }
+    };
+};
+#endif
